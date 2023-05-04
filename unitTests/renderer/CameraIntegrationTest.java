e587bd1f4f3fc0276038b3d58243ac7ef0c8d894
daniel_cohen <daniel1cohen13579@gmail.com>
2023-05-04 13:37:23 +0300

    integration test

---
+++ unitTests/renderer/CameraIntegrationTest.java
@@ -0,0 +1,11 @@
+package renderer;
+import org.junit.jupiter.api.Test;
+
+import geometries.*;
+import primitives.*;
+
+import java.util.LinkedList;
+import java.util.List;
+public class CameraIntegrationTest {
+
+}
