b1240ef2be4119f02ca4b6c0142b1f08c019028c
daniel_cohen <daniel1cohen13579@gmail.com>
2023-05-07 15:51:19 +0300

    almost done stage 5

--- src/renderer/RayTracerBasic.java
+++ src/renderer/RayTracerBasic.java
@@ -1,17 +1,25 @@
 package renderer;
 
 import primitives.Color;
+import primitives.Point;
 import primitives.Ray;
 import scene.Scene;
-
+import java.util.*;
 public class RayTracerBasic extends RayTracerBase{
-    public RayTracerBasic(Scene scene){
-        super(scene);
-    }
+    public RayTracerBasic(Scene scene){super(scene);}
 
     @Override
     public Color traceRay(Ray ray){
-        return null;
+        List<Point> pointList =  this.scene.geometries.findIntersections(ray);
+        if (pointList.size() == 0){
+            return scene.background;
+        }
+        Point closest = ray.findClosestPoint(pointList);
+        return calcColor(closest);
+    }
+
+    private Color calcColor(Point p){
+        return scene.ambientLight.getIntensity();
     }
 
 }
--- unitTests/renderer/RenderTests.java
+++ unitTests/renderer/RenderTests.java
@@ -24,7 +24,7 @@ public class RenderTests {
                                            new Double3(1, 1, 1))) //
          .setBackground(new Color(75, 127, 90));
 
-      scene.geometries.add(new Sphere(new Point(0, 0, -100), 50d),
+      scene.geometries.add(new Sphere(50d,new Point(0, 0, -100)),
                            new Triangle(new Point(-100, 0, -100), new Point(0, 100, -100), new Point(-100, 100, -100)), // up
                            // left
                            new Triangle(new Point(-100, 0, -100), new Point(0, -100, -100),
@@ -32,7 +32,7 @@ public class RenderTests {
                            // left
                            new Triangle(new Point(100, 0, -100), new Point(0, -100, -100), new Point(100, -100, -100))); // down
       // right
-      Camera camera = new Camera(Point.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+      Camera camera = new Camera(new Point(0,0,0), new Vector(0, 0, -1), new Vector(0, 1, 0)) //
          .setVPDistance(100) //
          .setVPSize(500, 500) //
          .setImageWriter(new ImageWriter("base render test", 1000, 1000))
@@ -78,21 +78,4 @@ public class RenderTests {
    // camera.writeToImage();
    // }
 
-   /** Test for XML based scene - for bonus */
-   @Test
-   public void basicRenderXml() {
-      Scene  scene  = new Scene("XML Test scene");
-      // enter XML file name and parse from XML file into scene object
-      // using the code you added in appropriate packages
-      // ...
-      // NB: unit tests is not the correct place to put XML parsing code
-
-      Camera camera = new Camera(Point.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0))     //
-         .setVPDistance(100)                                                                //
-         .setVPSize(500, 500).setImageWriter(new ImageWriter("xml render test", 1000, 1000))
-         .setRayTracer(new RayTracerBasic(scene));
-      camera.renderImage();
-      camera.printGrid(100, new Color(YELLOW));
-      camera.writeToImage();
-   }
 }
